47a8026f652eb3005d85952465b95a1e916a56b6
kamilx3 <kamilx3@gmail.com>
2015-02-02 14:25:58 +0100

    Updated version to 1.3.0

--- src/main/java/info/jbcs/minecraft/safe/Safe.java
+++ src/main/java/info/jbcs/minecraft/safe/Safe.java
@@ -27,7 +27,7 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraftforge.common.config.Configuration;
 
 
-@Mod(modid = "Safe", name = "Safe", version = "1.2.0") //dependencies = "required-after:Autoutils"
+@Mod(modid = "Safe", name = "Safe", version = "1.3.0") //dependencies = "required-after:Autoutils"
 public class Safe {
 	public static BlockSafe blockSafe;
 	public static GuiHandler guiSafe;
