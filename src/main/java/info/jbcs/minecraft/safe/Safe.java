23230aa5ca30d8fef4e05fd90738ba242f45ba80
Kamil Danak <kamilx3@gmail.com>
2015-07-22 17:49:02 +0200

    Version bump

--- src/main/java/info/jbcs/minecraft/safe/Safe.java
+++ src/main/java/info/jbcs/minecraft/safe/Safe.java
@@ -27,7 +27,7 @@ import net.minecraftforge.common.config.Configuration;
 import java.io.File;
 
 
-@Mod(modid = "Safe", name = "Safe", version = "1.3.0") //dependencies = "required-after:Autoutils"
+@Mod(modid = "Safe", name = "Safe", version = "1.3.1") //dependencies = "required-after:Autoutils"
 public class Safe {
 	public static BlockSafe blockSafe;
 	public static GuiHandler guiSafe;
